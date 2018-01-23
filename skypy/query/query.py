8176b9c8238b976cd6535bf783fc811bd88837d2
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2018-01-23 19:39:18 +0000

    adding workflow for general query;

--- skypy/query/query.py
+++ skypy/query/query.py
@@ -23,15 +23,35 @@ assert assertion()
 __version__ = package_version()
 
 
-def general():
+def simbadoquery(qon):
+    '''
+    will do a general object query of simbad
+    '''
+    s = Simbad()
+    s.add_votable_fields('id(1)','id(2)','ra(d)','dec(d)','ubv')
+    r = s.query_object(qon)
+    # need to add slicer to grab only needed data
+    return r.as_array()
 
->>> s = Simbad()
->>> s.add_votable_fields('id(1)','id(2)','ra(d)','dec(d)','ubv')
->>> r = s.query_object('m31')
->>> r
->>> r.as_array()
+def simbadrquery(qon):
+    '''
+    will do a general region query of simbad
+    '''
+    s = Simbad()
+    s.add_votable_fields('id(1)','id(2)','ra(d)','dec(d)','ubv')
+    r = s.query_object(qon)
+    # need to add slicer to grab only needed data
+    return r.as_array()
 
+def vizierquery(qon):
+    '''
+    returns standard formatted date
+    '''
+    return
 
-
-
-
+def organize(oarray):
+    '''
+    organize the data output
+    '''
+    # take the data input and put into the list of tuples or tuple objects
+    return
