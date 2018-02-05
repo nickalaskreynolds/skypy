88b8e8850aaa1b944fc5c947687c9acb198e7216
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2018-02-05 22:00:07 +0000

    adding sqls and generator

---
+++ database/generatesql.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import numpy as np
+import sys
+
+try:
+    stars = np.loadtxt('starlist.txt.backup',skiprows=11,dtype=str)
+except:
+    print('If file not found, go to https://github.com/nickalaskreynolds/skypy.git/database/ and grab file')
+    sys.exit(1)
+
+try:
+    con = lite.connect('star.sql')
+    cur = con.cursor()   
+    cur.execute('DROP TABLE IF EXISTS Stars')
+    cur.execute('CREATE TABLE Stars(Name1 TEXT, Name2 TEXT, Type TEXT, RA TEXT, DEC TEXT, EPOCH INT, FluxT TEXT, FluxV FLOAT,lastupdate INT)')
+    for x in stars: 
+        #print(len(x))
+        #print(x)
+        cur.execute("INSERT INTO Stars VALUES('{}','{}','{}','{}','{}',{},'{}',{},{})".format(x[0],x[1],x[2],x[3],x[4],int(x[5]),x[6],float(x[7]),int(x[8])))
+
+    con.commit()
+    
+except lite.Error as e:
+    
+    if con:
+        con.rollback()
+        
+    print("Error %s:" % e.args[0])
+    sys.exit(1)
+    
+finally:
+    
+    if con:
+        con.close() 
+
+con = lite.connect('star.sql')
+cur = con.cursor()   
+with con:    
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM Stars")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+
+try:
+    locations = np.loadtxt('location.txt.backup',skiprows=3,dtype=str)
+except:
+    print('If file not found, go to https://github.com/nickalaskreynolds/skypy.git/database/ and grab file')
+    sys.exit(1)
+
+try:
+    con = lite.connect('location.sql')
+    cur = con.cursor()   
+    cur.execute('DROP TABLE IF EXISTS Locations')
+# Name             LAT(N)      LONG(W)    UTC DST(Y/N) LastUpdated
+    cur.execute('CREATE TABLE Locations(Name1 TEXT, Lat TEXT, Long TEXT, UTC INT, DST TEXT,lastupdate INT)')
+    for x in locations: 
+        #print(len(x))
+        #print(x)
+        cur.execute("INSERT INTO Locations VALUES('{}','{}','{}',{},'{}',{})".format(x[0],x[1],x[2],int(x[3]),x[4],int(x[5])))
+
+    con.commit()
+    
+except lite.Error as e:
+    
+    if con:
+        con.rollback()
+        
+    print("Error %s:" % e.args[0])
+    sys.exit(1)
+    
+finally:
+    
+    if con:
+        con.close() 
+
+con = lite.connect('location.sql')
+cur = con.cursor()   
+with con:    
+    cur = con.cursor()    
+    cur.execute("SELECT * FROM Locations")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
