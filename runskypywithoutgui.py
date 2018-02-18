f90c60b4f4c0a9b6af0d3cf126efb76f74cb2917
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-02-18 18:42:12 +0000

    adding workflow

---
+++ runskypywithoutgui.py
@@ -0,0 +1,3 @@
+# make a first run program which asks users for number of cores (or #-1) to begin chugging through stars
+# will update the database with the times the stars are available in a given location
+name the file, database.location.sql
