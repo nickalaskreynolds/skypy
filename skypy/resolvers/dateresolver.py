3c846760811dacb879efa1b042a5014d330b741a
Nickalas Reynolds <nickalaskreynolds@gmail.com>
2018-03-19 04:18:54 +0000

    fixing and making more general

--- skypy/resolvers/dateresolver.py
+++ skypy/resolvers/dateresolver.py
@@ -7,14 +7,14 @@ Misc  : Resolves the date supplied to a more standard format, yields
         an object that holds the orig date, utc, and the season
 '''
 
-# CHECK THIS!!!!!!!!
-
 # imported standard modules
-from collections import Iterable
 from datetime import date as today
+import datetime
+import math
 
 # import custom modules
 from ..version import *
+from ..misc.functions import *
 
 # checking python version
 assert assertion()
@@ -71,7 +71,7 @@ class date(object):
         seasonkey = str(month//2 + month%2)
         return seasons[seasonkey][2]
 
-def checkconv(obj):
+def checkconv(obj,epoch='ordinal'):
     '''
     will take a obj and convert to list proper
     probably more complicated than ever needs to be
@@ -79,7 +79,7 @@ def checkconv(obj):
     delimiters = [':',' ','/',',','.']
     # delimiting scheme assumes YYYY,MM,DD format
     # if obj is number of days since epoch, Julian(1950) or Ordinal(2001)
-    if (type(obj) == float) or (type(obj) == int):
+    if (type(obj) == float) or (type(obj) == int): 
         try:
             # try if int object is already in proper format
             temp = str(obj)
@@ -91,8 +91,11 @@ def checkconv(obj):
                 return obj
         except:
             pass
-        print('Assuming time since Ordinal (2001)')
-        fin = int(''.join([x for x in today.fromordinal(obj).timetuple()[0:3]]))
+        if epoch == 'ordinal':
+            fin = int(''.join([x for x in today.fromordinal(obj).timetuple()[0:3]]))
+        elif epoch == 'julian':
+            fin = datetime.datetime(2000, 1, 1) + datetime.timedelta(obj)
+            fin = int(''.join([x for x in fin.timetuple()[0:3]]))
         # returns int
         return fin
     # if object is string
@@ -109,8 +112,11 @@ def checkconv(obj):
             pass
         # check if float hidden as string
         try:
-            fin = int(''.join([x for x in today.fromordinal(float(obj)).timetuple()[0:3]]))
-            print('Assuming time since Ordinal (2001)')
+            if epoch == 'ordinal':
+                fin = int(''.join([x for x in today.fromordinal(float(obj)).timetuple()[0:3]]))
+            elif epoch == 'julian':
+                fin = datetime.datetime(2000, 1, 1) + datetime.timedelta(obj)
+                fin = int(''.join([x for x in fin.timetuple()[0:3]]))
             return fin
         except:
             # handle string proper
@@ -149,8 +155,196 @@ def checkconv(obj):
                 print('Date object incorrect format')
                 return False
 
-def typecheck(obj): 
+def convertdatejdn(date,time,method=1):
+    date = str(date)
+    time = str(time)
+    """
+    1. Get current values for year, month, and day
+    2. Same for time and make it a day fraction
+    3. Calculate the julian day number via   https://en.wikipedia.org/wiki/Julian_day
+    4. Add the day fraction to the julian day number
+
+    """
+    MJD_0 = 2400000.5
+    MJD_JD2000 = 51544.5
+
+    def fpart(x):
+        """Return fractional part of given number."""
+        return math.modf(x)[0]
+
+
+    def ipart(x):
+        """Return integer part of given number."""
+        return math.modf(x)[1]
+
+
+    def is_leap(year):
+        """Leap year or not in the Gregorian calendar."""
+        x = math.fmod(year, 4)
+        y = math.fmod(year, 100)
+        z = math.fmod(year, 400)
+
+        # Divisible by 4 and,
+        # either not divisible by 100 or divisible by 400.
+        return not x and (y or not z)
+
+
+    def gcal2jd(year, month, day):
+        year = int(year)
+        month = int(month)
+        day = int(day)
+
+        a = ipart((month - 14) / 12.0)
+        jd = ipart((1461 * (year + 4800 + a)) / 4.0)
+        jd += ipart((367 * (month - 2 - 12 * a)) / 12.0)
+        x = ipart((year + 4900 + a) / 100.0)
+        jd -= ipart((3 * x) / 4.0)
+        jd += day - 2432075.5  # was 32075; add 2400000.5
+
+        jd -= 0.5  # 0 hours; above JD is for midday, switch to midnight.
+
+        return MJD_0, jd
+
+
+    def jd2gcal(jd1, jd2):
+        from math import modf
+
+        jd1_f, jd1_i = modf(jd1)
+        jd2_f, jd2_i = modf(jd2)
+
+        jd_i = jd1_i + jd2_i
+
+        f = jd1_f + jd2_f
+
+        # Set JD to noon of the current date. Fractional part is the
+        # fraction from midnight of the current date.
+        if -0.5 < f < 0.5:
+            f += 0.5
+        elif f >= 0.5:
+            jd_i += 1
+            f -= 0.5
+        elif f <= -0.5:
+            jd_i -= 1
+            f += 1.5
+
+        l = jd_i + 68569
+        n = ipart((4 * l) / 146097.0)
+        l -= ipart(((146097 * n) + 3) / 4.0)
+        i = ipart((4000 * (l + 1)) / 1461001)
+        l -= ipart((1461 * i) / 4.0) - 31
+        j = ipart((80 * l) / 2447.0)
+        day = l - ipart((2447 * j) / 80.0)
+        l = ipart(j / 11.0)
+        month = j + 2 - (12 * l)
+        year = 100 * (n - 49) + i + l
+
+        return int(year), int(month), int(day), f
+
+
+    def jcal2jd(year, month, day):
+        year = int(year)
+        month = int(month)
+        day = int(day)
+
+        jd = 367 * year
+        x = ipart((month - 9) / 7.0)
+        jd -= ipart((7 * (year + 5001 + x)) / 4.0)
+        jd += ipart((275 * month) / 9.0)
+        jd += day
+        jd += 1729777 - 2400000.5  # Return 240000.5 as first part of JD.
+
+        jd -= 0.5  # Convert midday to midnight.
+
+        return MJD_0, jd
+
+
+    def jd2jcal(jd1, jd2):
+        from math import modf
+
+        jd1_f, jd1_i = modf(jd1)
+        jd2_f, jd2_i = modf(jd2)
+
+        jd_i = jd1_i + jd2_i
+
+        f = jd1_f + jd2_f
+
+        # Set JD to noon of the current date. Fractional part is the
+        # fraction from midnight of the current date.
+        if -0.5 < f < 0.5:
+            f += 0.5
+        elif f >= 0.5:
+            jd_i += 1
+            f -= 0.5
+        elif f <= -0.5:
+            jd_i -= 1
+            f += 1.5
+
+        j = jd_i + 1402.0
+        k = ipart((j - 1) / 1461.0)
+        l = j - (1461.0 * k)
+        n = ipart((l - 1) / 365.0) - ipart(l / 1461.0)
+        i = l - (365.0 * n) + 30.0
+        j = ipart((80.0 * i) / 2447.0)
+        day = i - ipart((2447.0 * j) / 80.0)
+        i = ipart(j / 11.0)
+        month = j + 2 - (12.0 * i)
+        year = (4 * k) + n + i - 4716.0
+
+        return int(year), int(month), int(day), f
+
+    y,mo,d,h,m,s= int(date[0:4]),int(date[4:6]),\
+                 int(date[6:8]),int(time[0:2]),\
+                 int(time[2:4]),int(time[4:6])
+    return sum(gcal2jd(y,mo,d)) + h / 24. + m / (24. * 60.) + s / (24. * 60. * 60.)
+
+def lst(jd,longit):
     '''
-    checks type of obj and verifies it is iterable proper
+    jd is wanted lst julian date
+    jd is float
+    jd0 is previous julian date midnight (must be 0.5)
+    h is hours since previous
+    longit is in hours
     '''
-    return not isinstance(obj, str) and isinstance(obj, Iterable)
+    # find julian date of previous midnight
+    if (jd % 1 ) < 0.5:
+        jd0 = float(int(jd)) - 0.5
+    else:
+        jd0 = float(int(jd)) + 0.5
+
+    MJD_0 = 2400000.5
+    MJD_JD2000 = 51544.5
+    h    = jd - jd0             # get hours since prev jd midnight
+    mjd  = MJD_JD2000 + MJD_0   # mean julian day conv
+    d,d0 = jd - mjd,jd0 - mjd   # num of days since j2000
+    t    = d / 36525            # number of centuries since year 2000
+    G = 6.697374558 #constant for year
+    GMST = G + 0.06570982441908 * d0 +\
+           1.00273790935 * h + 0.000026 * t**2 # greenwish mean sidereal time
+    print(GMST)       
+    GMST = GMST % 24       
+    ep   = 23.4393 - 0.0000004 * d # obliquity
+    loso = 280.47 + 0.98565 * d    # mean long of sun
+    lan  = 125.04 - 0.052954 * d   # line of ascending node of moon
+    ep,loso,lan = deg_2_rad([ep,loso,lan])
+    nuta = -0.000319 * math.sin(lan) - \
+            0.000024* math.sin(2.*loso) # nutation of logitude
+    eqeq = nuta * math.cos(ep) # equation of equinoxes
+    GAST = GMST + eqeq
+
+    longit = longit / 15.
+
+    # west justified
+    print('d0:{},h:{},t:{},gmst:{},eq:{},gast:{},long:{}'\
+          .format(d0,h,t,GMST,eqeq,GAST,longit))
+    lst = GAST - longit
+    lst0 = int(lst)
+    delt = (lst - lst0) * 60.
+    lst1 = int((lst - lst0) * 60.)
+    lst2 = (delt - lst1) * 60.
+    return lst,':'.join(map(str,[lst0,lst1,lst2]))
+
+
+    
+
+
+
--- skypy/resolvers/locationresolver.py
+++ skypy/resolvers/locationresolver.py
@@ -7,11 +7,9 @@ Misc  : Resolves the location supplied to a more standard format, yields
         an object that holds the orig date, utc, and the season
 '''
 
-# imported standard modules
-from collections import Iterable
-
 # import custom modules
 from ..version import *
+from ..misc.functions import *
 
 # checking python version
 assert assertion()
@@ -60,49 +58,3 @@ class location(object):
         lat = checkconv(lat)
         lon = checkconv(lon)
         return lat,lon
-
-def checkconv(coord):
-    '''
-    will take a coord convert to a list
-    return the decimal conversion
-    '''
-    delimiters = [':',' ','  ',',']
-    # if a string, handle
-    if type(coord) == float:
-        return coord
-    if type(coord) == str:
-        # check if float hidden as string
-        try:
-            return float(coord)
-        except:
-            # handle string proper
-            count = 0
-            while not typecheck(coord):
-                coord = coord.split(delimiters[count])
-                if len(coord) == 1:
-                    coord = coord[0]
-                count += 1
-    # if list, make to float, final form
-    if typecheck(coord):
-        if len(coord) == 3:
-            temp0,temp1,temp2 = map(float,coord)
-            total = abs(temp0) + temp1/60. + temp2/3600
-        elif len(coord) == 2:
-            temp0,temp1 = map(float,coord)
-            temp2 = temp1/60. - temp1
-            total = abs(temp0) + temp1/60. + temp2/3600
-        elif len(coord) == 1:
-            # if an iterable of len one with float
-            try:
-                return float(coord[0])
-            except:
-                return checkconv(coord[0])
-        if temp0 < 0.:
-            total = total * -1
-    return total
-
-def typecheck(obj): 
-    '''
-    checks type of obj and verifies it is iterable proper
-    '''
-    return not isinstance(obj, str) and isinstance(obj, Iterable)
